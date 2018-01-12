489244457c5f37dda6dbc83bc8c60ec58b144226
developer <developer@DESKTOP-L75S7D1>
2018-01-11 19:07:02 -0600

    no idea if this is right

---
+++ Favoritethings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Favoritethings
+{
+    class Dogs
+    {
+        protected string _dogName;
+        public string Dogname;
+        {
+            get
+            {
+                return _dogName;
+            }
+            set
+            {
+                _dogName = value;
+            }
+        }
+    }
+    class Cats
+{
+    protected string _catName;
+    public string Catname; 
+    {
+        get 
+        {
+        return _catName
+        }
+        set 
+        {
+        _catName = value;
+         }
+    }
+    }
+}
